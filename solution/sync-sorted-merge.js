d15054970b3b010da08dd154b1e394eff059bc30
Gonen Shoham <5544864+gonensh@users.noreply.github.com>
2024-09-23 23:52:53 -0700

    refactor: relocate drainHeap for cleanliness

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,28 +3,31 @@ const { Heap } = require('heap-js');
 
 const DEBUG = process.env.DEBUG;
 
+const drainHeap = ({ heap, maxDate, sizeThreshold = 5, printer }) => {
+  // Break if heap size is under threshold
+  if (heap.size() < sizeThreshold) return;
+  // Debug log
+  if (DEBUG) {
+    console.log(
+      `draining heap. size: ${heap.size()} maxDate: ${maxDate?.toISOString?.()} sizeThreshold: ${sizeThreshold}`
+    );
+  }
+  let i = 0;
+  // Print out all of the entries up to maxDate
+  while (!heap.isEmpty() && (!maxDate || heap.peek()?.date <= maxDate)) {
+    printer.print(heap.pop());
+    i++;
+  }
+  // Debug log
+  if (DEBUG) {
+    console.log(`drained log messages: ${i}`);
+  }
+};
+
 // Print all entries, across all of the sources, in chronological order.
 module.exports = (originalLogSources, printer) => {
   let logSources = [...originalLogSources];
   const heap = new Heap((a, b) => a.date.getTime() - b.date.getTime());
-
-  const drainHeap = (heap, maxDate, sizeThreshold = 5) => {
-    // Break if heap size is under threshold
-    if (heap.size() < sizeThreshold) return;
-    // Debug log
-    if (DEBUG)
-      console.log(
-        `draining heap. size: ${heap.size()} maxDate: ${maxDate?.toISOString?.()} sizeThreshold: ${sizeThreshold}`
-      );
-    let i = 0;
-    // Print out all of the entries up to maxDate
-    while (!heap.isEmpty() && (!maxDate || heap.peek()?.date <= maxDate)) {
-      printer.print(heap.pop());
-      i++;
-    }
-    // Debug log
-    if (DEBUG) console.log(`${i} log messages drained`);
-  };
 
   while (logSources.length > 0) {
     let minDateInBatch = new Date();
@@ -40,11 +43,11 @@ module.exports = (originalLogSources, printer) => {
     // Clear empty log sources
     logSources = logSources.filter((logSource) => !logSource.drained);
 
-    drainHeap(heap, minDateInBatch, 10);
+    drainHeap({ heap, maxDate: minDateInBatch, sizeThreshold: 10, printer });
   }
 
   // Drain the rest of the heap
-  drainHeap(heap, null, 0);
+  drainHeap({ heap, maxDate: null, sizeThreshold: 0, printer });
 
   printer.done();
   return console.log('Sync sort complete.');
