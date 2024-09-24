a1bc8a0e9b290882ceda06ba637a4ba88254fa7d
Gonen Shoham <5544864+gonensh@users.noreply.github.com>
2024-09-23 23:59:52 -0700

    feat: implement async sorted merge

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,66 @@
-"use strict";
+'use strict';
+
+const { Heap } = require('heap-js');
+
+const DEBUG = process.env.DEBUG;
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
+const drainHeap = ({ heap, maxDate, sizeThreshold = 5, printer }) => {
+  // Break if heap size is under threshold
+  if (heap.size() < sizeThreshold) return;
+  // Debug log
+  if (DEBUG) {
+    console.log(
+      `draining heap. size: ${heap.size()} maxDate: ${maxDate?.toISOString?.()} sizeThreshold: ${sizeThreshold}`
+    );
+  }
+  let i = 0;
+  // Print out all of the entries up to maxDate
+  while (!heap.isEmpty() && (!maxDate || heap.peek()?.date <= maxDate)) {
+    printer.print(heap.pop());
+    i++;
+  }
+  // Debug log
+  if (DEBUG) {
+    console.log(`drained log messages: ${i}`);
+  }
+};
+
+const processAsyncLogs = async (originalLogSources, printer) => {
+  let logSources = [...originalLogSources];
+  const heap = new Heap((a, b) => a.date.getTime() - b.date.getTime());
+
+  while (logSources.length > 0) {
+    let minDateInBatch = new Date();
+
+    const batch = logSources.map((logSource) => logSource.popAsync());
+    const entries = await Promise.allSettled(batch);
+
+    entries.forEach(({ value: entry }) => {
+      if (entry) {
+        if (entry && entry.date < minDateInBatch) minDateInBatch = entry.date;
+        heap.add(entry);
+      }
+    });
+
+    // Clear empty log sources
+    logSources = logSources.filter((logSource) => !logSource.drained);
+
+    drainHeap({ heap, maxDate: minDateInBatch, sizeThreshold: 10, printer });
+  }
+
+  // Drain the rest of the heap
+  drainHeap({ heap, maxDate: null, sizeThreshold: 0, printer });
+
+  printer.done();
+  return true;
+};
+
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    processAsyncLogs(logSources, printer).then(() => {
+      resolve(console.log('Async sort complete.'));
+    });
   });
 };
